1f920d8bb5460a37525576edf400dd0c847152b2
Dan Bravender <dan.bravender@gmail.com>
2010-06-19 23:40:05 -0400

    Now tracks players with cookies

--- server.py
+++ server.py
@@ -18,12 +18,48 @@ game_factories = {
 }
 
 games = []
+users = {}
 
-class LobbyHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+            return self.get_secure_cookie("user")
+
+class LoginHandler(BaseHandler):
     def get(self):
-        self.write(loader.load('lobby.html').generate(games=games, game_factories=game_factories))
+        self.write('<html><body><form action="/login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        if not users.has_key(self.get_argument('name')):
+            users[self.get_argument('name')] = {}
+        self.redirect("/")
 
-class NewGameHandler(tornado.web.RequestHandler):
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.set_secure_cookie("user", "")
+        self.redirect('/')
+
+class QuitHandler(BaseHandler):
+    def get(self):
+        user = users[self.get_current_user()]
+        user['player'].left = True
+        del user['player']
+        del user['game']
+        self.redirect('/')
+
+class LobbyHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        if users[self.get_current_user()].has_key('game'):
+            game = users[self.get_current_user()]['game']
+            player = users[self.get_current_user()]['player']
+            self.redirect('/' + str(id(game)) + '/' + str(id(player)))
+            return
+        self.write(loader.load('lobby.html').generate(user=self.get_current_user(), games=games, game_factories=game_factories))
+
+class NewGameHandler(BaseHandler):
     def get(self):
         players = int(self.get_argument('players', 4))
         game_factory = game_factories[self.get_argument('game', 'crosspurposes')]
@@ -33,14 +69,16 @@ class NewGameHandler(tornado.web.RequestHandler):
         application.add_handlers(r'.*$', [(r'/' + str(id(game)), NewPlayerHandler, {'game': game})])
         self.redirect('/' + str(id(game)))
 
-class NewPlayerHandler(tornado.web.RequestHandler):
+class NewPlayerHandler(BaseHandler):
     def __init__(self, *args, **kwargs):
         self.game = kwargs['game']
         super(NewPlayerHandler, self).__init__(*args)
 
     def get(self):
-        player = self.game.player_factory('Player ' + str(len(self.game.players) + 1), game=self.game)
+        player = self.game.player_factory(self.get_current_user(), game=self.game)
         self.game.add_player(player, player)
+        users[self.get_current_user()]['player'] = player
+        users[self.get_current_user()]['game'] = self.game
         application.add_handlers(r'.*$',
             [(self.request.uri + '/' + str(id(player)),
              PlayerInfo,
@@ -55,7 +93,7 @@ class NewPlayerHandler(tornado.web.RequestHandler):
              {'player': player})])
         self.redirect(self.request.uri + '/' + str(id(player)))
 
-class PlayerInfo(tornado.web.RequestHandler):
+class PlayerInfo(BaseHandler):
     def __init__(self, *args, **kwargs):
         self.player = kwargs.pop('player')
         self.template = kwargs.pop('template')
@@ -89,11 +127,16 @@ class PlayerWebSocket(tornado.websocket.WebSocketHandler):
             traceback.print_exc()
         self.receive_message(self.on_message)
 
-settings = {'static_path': os.path.join(os.path.realpath(__file__ + '/../'), 'static')}
+settings = {'static_path'  : os.path.join(os.path.realpath(__file__ + '/../'), 'static'),
+            'cookie_secret': 'QU%9B4\'?E$@(D0Q($5?@()".8B&%UOD1M5Y.IMD',
+            'login_url'    : '/login'}
 
 application = tornado.web.Application(**settings)
 application.add_handlers('.*$', [(r'/', LobbyHandler)])
 application.add_handlers('.*$', [(r'/new', NewGameHandler)])
+application.add_handlers('.*$', [(r'/login', LoginHandler)])
+application.add_handlers('.*$', [(r'/logout', LogoutHandler)])
+application.add_handlers('.*$', [(r'/quit', QuitHandler)])
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)
